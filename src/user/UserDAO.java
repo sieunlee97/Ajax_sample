9a7c39ecbd589fe67bcc52e85d5f5c71677602ca
[이시은] <[tldms0137@naver.com]>
2021-06-28 17:05:59 +0900

    데이터베이스 연동하기

---
+++ src/user/UserDAO.java
@@ -0,0 +1,45 @@
+package user;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class UserDAO {
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	
+	public UserDAO() {
+		try {
+			String dbURL = "jdbc:mysql://localhost:3306/ajax";
+			String dbID="root";
+			String dbPassword="apmsetup";
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public ArrayList<User> search(String userName){
+		String SQL = "SELECT * FROM user WHERE userName LINK ?";
+		ArrayList<User> userList = new ArrayList<User>();
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userName);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				User user = new User();
+				user.setUserName(rs.getString(1));
+				user.setUserAge(rs.getInt(2));
+				user.setUserGender(rs.getString(3));
+				user.setUserEmail(rs.getString(4));
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return userList;
+	}
+}
